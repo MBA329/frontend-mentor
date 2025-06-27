dd15c8a486f8604c2631e8ae70083d0702823fb3
emmanuel mba <emmanuelmba329@gmail.com>
2025-06-27 21:37:39 +0100

    random commit

--- space-tourism-website/space-tourism-website/src/routes/index.tsx
+++ space-tourism-website/space-tourism-website/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layout";
 import Home from "../pages/Home";
+import Crew from "../pages/Crew";
 
 
 const router = createBrowserRouter([
@@ -9,7 +10,10 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {index:true,element:<Home/>
-      }
+      },
+      {path:"/crew",element:<Crew/>},
     ]
   }
 ])
+
+export default router;
